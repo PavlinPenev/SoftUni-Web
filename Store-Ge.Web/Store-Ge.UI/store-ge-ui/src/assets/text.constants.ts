f43035bb5145e3b7ab7790cb95243963d33ec9d9
Paf <pavlinpenevv@abv.bg>
2022-10-16 10:11:08 +0300

    Front-end - Home page design

---
+++ Store-Ge.Web/Store-Ge.UI/store-ge-ui/src/assets/text.constants.ts
@@ -0,0 +1,11 @@
+export const HOME = 'Home';
+export const LOGIN = 'Login';
+export const REGISTER = 'Register';
+export const FOOTER_TEXT = 'Store-Ge® 2022 -';
+
+export const APP_NAME = "Store-Ge";
+export const INFO_TEXT_1 = 'Register all your shops, stores, cafes etc. All of them organized in one application.';
+export const INFO_TEXT_2 = 'Add your products from your orders. Keep documentation of the orders. Easily access them whenever you want!';
+export const INFO_TEXT_3 = 'Add cashiers to your shops and make sales. Cashiers use our application easy and safe.';
+export const INFO_TEXT_4 = 'Keep track of your suppliers as well! We have a post-order process allowing you to add your suppliers before adding orders/products.'
+export const INFO_TEXT_5 = 'Use our application with ease. Every feature is easily accessible and user-friendly. Application`s goal is to make you as productive as you can be!';
